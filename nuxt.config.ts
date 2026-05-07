72537f164809a291222df16527bb224fd0d3c375
Fly0w <florian.budniewski@hotmail.com>
2026-05-07 22:18:35 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@nuxtjs/i18n',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
