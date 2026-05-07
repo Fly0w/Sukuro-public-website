1644626c6c96bcbba3b34790dc9e952e39b051f7
Fly0w <florian.budniewski@hotmail.com>
2026-05-07 22:29:18 +0900

    shadcn install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/icon',
     '@nuxt/fonts',
     '@nuxt/eslint',
     '@nuxtjs/i18n',
+    'shadcn-nuxt',
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+        'class-variance-authority',
+        'reka-ui',
+        'clsx',
+        'tailwind-merge',
+        'lucide-vue-next',
+      ]
+    }
+  },
+  css: ['~/assets/css/tailwind.css'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
+  },
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    strategy: 'prefix_except_default',
+    vueI18n: './i18n.config.ts',
+  },
 })
